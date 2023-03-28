8ebda8b9c8636f0a2fb5e54f0d84bd9297dfd752
= <=>
2023-03-28 13:41:27 +0100

    internationalization added

---
+++ i18n.ts
@@ -0,0 +1,10 @@
+// import { NextI18Next } from 'next-i18next';
+
+
+// const NextI18NextInstance = new NextI18Next({
+//   defaultLanguage: 'en',
+//   otherLanguages: ['es'],
+//   localePath: 'public/static/locales',
+// });
+
+// export default NextI18NextInstance;
