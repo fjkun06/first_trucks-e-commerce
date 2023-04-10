14fe30568e7a5fe777685d60651baff1587528ee
= <=>
2023-04-10 14:00:49 +0100

    navbar good

--- first_trucks_sarl/src/stories/Navbar.tsx
+++ first_trucks_sarl/src/stories/Navbar.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { AnimatePresence, motion, animate, Variants } from "framer-motion";
+import { AnimatePresence, motion, animate, Variants, LayoutGroup } from "framer-motion";
 import Image from "next/image";
 import logo from "../../public/images/logoM.png";
 import Open from "../../public/svg/Open";
@@ -23,22 +23,38 @@ const Navbar: React.FC<NavbarProps> = ({ opened }) => {
 
   return (
     <>
-      <AnimatePresence>
-        <motion.nav layout key="1" animate={{height: isOpen? '19.5rem' : '6.6rem'}}>
-          <motion.div layout>
+      <LayoutGroup>
+        {/* <motion.nav layout animate={{ scaleY: isOpen ? 1.5 : 1 }} > */}
+        {/* <motion.nav layout animate={{ height: isOpen ? 195 : 66 }} > */}
+        <motion.nav layout animate={{ height: isOpen ? "19.5rem" : "6.6rem" }} transition={{ duration: 0.25 }}>
+          <motion.div id="nav1">
+            {/* <motion.div layout id="nav1" animate={{ height: isOpen ? "9.75rem" : "6.6rem" }}> */}
             <Image src={logo} alt="logo" />
             <Menu isOpen={isOpen} handler={handleClick} />
           </motion.div>
-          <motion.div layout animate={{scale: isOpen? 1 : 0,display: isOpen? "flex":"none"}}>
-            <motion.span>
-              À Propos de Nous
-              {/* <Menu/> */}
-            </motion.span>
-            <Button />
-          </motion.div>
+          <AnimatePresence>
+            {isOpen && (
+              <motion.div
+                id="nav2"
+                layout
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ stiffness: 100, delay: 0.65, exit: {duration: 0.4, delay: 0.2} }}
+                exit={{ opacity: 0, transition: { duration: 0.7, delay: 0.25 } }}
+              >
+                {/* <motion.div id="nav2" layout initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ stiffness: 100, delay: 0.65 }} exit={{  scaleY: 0 }}> */}
+                <motion.span>
+                  {/* <motion.span animate={{ opacity: isOpen ? 1 : 0 }} transition={{ delay: 1 }}> */}À Propos de Nous
+                  {/* <Menu/> */}
+                </motion.span>
+                <Button />
+              </motion.div>
+            )}
+          </AnimatePresence>
         </motion.nav>
-        <motion.div id="modal" />
-      </AnimatePresence>
+      </LayoutGroup>
+
+      <motion.div id="modal" />
     </>
   );
 };
