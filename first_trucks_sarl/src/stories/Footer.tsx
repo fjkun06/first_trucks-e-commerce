3833f268fd944ceaf3b593024d636507df21d958
= <=>
2023-04-12 00:27:07 +0100

    Completed second child

--- first_trucks_sarl/src/stories/Footer.tsx
+++ first_trucks_sarl/src/stories/Footer.tsx
@@ -1,5 +1,7 @@
 import Button from "./Button";
 import diamondBG from "../../public/images/blurM.png";
+import engine from "../../public/images/engine.png";
+import logoWText from "../../public/svg/logoWtext.svg";
 import Image from "next/image";
 
 const Footer = () => {
@@ -8,13 +10,38 @@ const Footer = () => {
       <section className="diamond">
         <Button />
         <Image src={diamondBG} alt="diamond background" />
-
       </section>
-      <div>
-        <section>1</section>
-        <section>2</section>
-        <section>3</section>
-      </div>
+      <section className="footer">
+        <div className="footer__menu">
+          <section>
+            <Image src={logoWText} alt="logo" />
+            <Image src={engine} alt="engine" />
+          </section>
+          <section>
+            <div className="footer__contact1">
+              <span>Nous sommes à</span>
+              <div className="footer__contact__town">
+                <span>Yaoundé</span>
+                <span>Douala</span>
+                <span>Bafoussam</span>
+              </div>
+            </div>
+            <div className="footer__contact2">
+              <span>Contactez-nous</span>
+              <div className="footer__contact__town">
+                <span>+237 671 234 567</span>
+                <span>+237 671 234 567</span>
+                <span>info.nous@firsttruckssarl.cm</span>
+              </div>
+            </div>
+          </section>
+          <section>
+            <span>child 1</span>
+            <div>child 2</div>
+          </section>
+        </div>
+        <span>2023 FISRT TRUCKS SARL</span>
+      </section>
     </footer>
   );
 };
