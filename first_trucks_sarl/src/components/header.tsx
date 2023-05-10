4a088df6c4b337376bc85c30129cec62ee9db452
= <=>
2023-05-10 10:10:12 +0100

    optimized images

--- first_trucks_sarl/src/components/header.tsx
+++ first_trucks_sarl/src/components/header.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import headerMob from "../../public/images/hdr_mob.png";
+import headerMob from "../../public/svg/hdr_mob.svg";
 import headerTab from "../../public/images/hdr_tab.png";
 import headerDesk from "../../public/images/hdr_desk.png";
-import item1M from "../../public/images/i1m.png";
-import item2M from "../../public/images/i2m.png";
-import item3M from "../../public/images/i3m.png";
-import item4M from "../../public/images/i4m.png";
+import item1M from "../../public/svg/i1m.svg";
+import item2M from "../../public/svg/i2m.svg";
+import item3M from "../../public/svg/i3m.svg";
+import item4M from "../../public/svg/i4m.svg";
 import Image from "next/image";
 import Button from "@/stories/Button";
 import { motion, Variants } from "framer-motion";
@@ -14,18 +14,19 @@ const Header = () => {
   return (
     <header>
       <div className="hdr_image">
-        <Image src={headerMob} alt="logo" priority />
-        <Image src={headerTab} alt="logo" priority />
-        <Image src={headerDesk} alt="logo" priority />
+        <Image src={headerMob} alt="logo" priority quality={100}/>
+        <Image src={headerTab} alt="logo" priority quality={100}/>
+        <Image src={headerDesk} alt="logo" priority quality={100}/>
       </div>
       <div className="hdr_content">
-        <div>
+        <div className="hdr_content_text">
           <span className="">Faites confiance à First Trust Sarl pour des pièces automobiles, lubrifiants moteur, et fluid de qualité.</span>
           <Button />
         </div>
         <div className="hdr_content_image">
           <motion.span initial={{x:"12rem"}} animate={{x:"3.5rem" }}  transition={{delay:1.5,duration:0.35}}>
-            <Image src={item1M} alt="image" />
+            {/* <img src={} alt="image" /> */}
+            <Image src={item1M} alt="image" width={56} height={37}/>
           </motion.span>
 
           <motion.span initial={{x:"12rem"}} animate={{x:"-3.5rem" }}  transition={{delay:1,duration:0.35}}>
