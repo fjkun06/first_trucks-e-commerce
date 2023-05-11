a79ba25c3fa36d3b2c0b491c46ee8f103219dc6d
= <=>
2023-05-11 20:22:09 +0100

    tablet header good

--- first_trucks_sarl/src/components/header.tsx
+++ first_trucks_sarl/src/components/header.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import headerMob from "../../public/images/hdr_mob.webp";
-import headerTab from "../../public/images/hdr_tab.png";
+import headerTab from "../../public/svg/hdr_tab2.svg";
+// import headerTab from "../../public/images/hdr_tab.png";
 import headerDesk from "../../public/images/hdr_desk.webp";
+// import item1M from "../../public/svg/i1m.svg";
 import item1M from "../../public/images/i1m.webp";
 import item2M from "../../public/images/i2m.webp";
 import item3M from "../../public/images/i3m.webp";
@@ -10,8 +12,12 @@ import item4M from "../../public/images/i4m.webp";
 import Image from "next/image";
 import Button from "@/stories/Button";
 import { motion, Variants } from "framer-motion";
+import useMediaQuery from "@/hooks/useMediaQuery";
 
 const Header = () => {
+  const tablet = useMediaQuery("(width > 800px)");
+  const desktop = useMediaQuery("(width > 800px)");
+
   return (
     <header>
       <div className="hdr_image">
@@ -27,18 +33,21 @@ const Header = () => {
           <Button />
         </div>
         <div className="hdr_content_image">
-          <motion.span initial={{ x: "12rem" }} animate={{ x: "3.5rem" }} transition={{ delay: 1.5, duration: 0.35 }}>
-            {/* <img src={} alt="image" /> */}
-            <Image src={item1M} alt="image" width={56} height={37} />
+          <motion.span initial={{ x: "22rem" }} animate={{ x: "7.5rem" }} transition={{ delay: 1.5, duration: 0.5 }}>
+          {/* <motion.span initial={{ x: "12rem" }} animate={{ x: "3.5rem" }} transition={{ delay: 1.5, duration: 0.35 }}> */}
+            <Image src={item1M} alt="image" />
           </motion.span>
 
-          <motion.span initial={{ x: "12rem" }} animate={{ x: "-3.5rem" }} transition={{ delay: 1, duration: 0.35 }}>
+          <motion.span initial={{ x: "20rem" }} animate={{ x: "-6.5rem" }} transition={{ delay: 1, duration: 0.5 }}>
+          {/* <motion.span initial={{ x: "12rem" }} animate={{ x: "-3.5rem" }} transition={{ delay: 1, duration: 0.35 }}> */}
             <Image src={item2M} alt="image" />
           </motion.span>
-          <motion.span initial={{ x: "12rem" }} animate={{ x: "-1rem" }} transition={{ duration: 0.35 }}>
+          <motion.span initial={{ x: "22rem" }} animate={{ x: "-2rem" }} transition={{ duration: 0.5 }}>
+          {/* <motion.span initial={{ x: "12rem" }} animate={{ x: "-1rem" }} transition={{ duration: 0.35 }}> */}
             <Image src={item3M} alt="image" />
           </motion.span>
-          <motion.span initial={{ y: "10rem" }} animate={{ y: "-1.45rem" }} style={{ x: "-8rem" }} transition={{ delay: 0.5, duration: 0.35 }}>
+          <motion.span initial={{ y: "15.5rem" }} animate={{ y: "-7.45rem" }} style={{ x: "-12rem" }} transition={{ delay: 0.5, duration: 0.5 }}>
+          {/* <motion.span initial={{ y: "10rem" }} animate={{ y: "-1.45rem" }} style={{ x: "-8rem" }} transition={{ delay: 0.5, duration: 0.35 }}> */}
             <Image src={item4M} alt="image" />
           </motion.span>
         </div>
