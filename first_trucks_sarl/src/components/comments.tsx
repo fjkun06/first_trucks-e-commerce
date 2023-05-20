d0a2c6829db754306004e002051a5dcf9aad7946
= <=>
2023-05-20 15:22:18 +0100

    text entrance animation

--- first_trucks_sarl/src/components/comments.tsx
+++ first_trucks_sarl/src/components/comments.tsx
@@ -1,60 +1,64 @@
+"use client";
 import { useTranslations } from "next-intl";
 import React from "react";
 import Heading from "./heading";
+import { motion, useInView } from "framer-motion";
+import { nanoid } from "nanoid";
 
 const Comments = () => {
-  const t = useTranslations("about");
-
+  const t = useTranslations("comments");
+  const ref = React.useRef(null);
+  const isInView = useInView(ref, { amount: 0.5 });
+  React.useEffect(() => {
+    console.log("Element is in view: ", isInView);
+  }, [isInView]);
+  const users = [
+    {
+      delay: 0,
+      direction: "left",
+    },
+    {
+      delay: 0.5,
+      direction: "right",
+    },
+    {
+      delay: 1,
+      direction: "left",
+    },
+    {
+      delay: 1.5,
+      direction: "right",
+    },
+  ];
   return (
     <section className="comments">
       <Heading text={t("title")} />
-      <div>
-        {/* <Comment />
-        <Comment />
-        <Comment />
-        <Comment /> */}
-        <p>
-          <span className="user_comment">
-            J'utilise les huiles moteur et les filtres de chez Future Trucks depuis des années et je n'ai jamais été déçu. Leurs produits m'ont aidé à assurer le bon fonctionnement et l'efficacité de
-            mon véhicule, et leurs prix sont très abordable.
-          </span>
-          <span className="user_name">Mr. NJOLEU Charles</span>
-        </p>
-        <p>
-          <span className="user_comment">
-            J'utilise les huiles moteur et les filtres de chez Future Trucks depuis des années et je n'ai jamais été déçu. Leurs produits m'ont aidé à assurer le bon fonctionnement et l'efficacité de
-            mon véhicule, et leurs prix sont très abordable.
-          </span>
-          <span className="user_name">Mr. NJOLEU Charles</span>
-        </p>
-        <p>
-          <span className="user_comment">
-            J'utilise les huiles moteur et les filtres de chez Future Trucks depuis des années et je n'ai jamais été déçu. Leurs produits m'ont aidé à assurer le bon fonctionnement et l'efficacité de
-            mon véhicule, et leurs prix sont très abordable.
-          </span>
-          <span className="user_name">Mr. NJOLEU Charles</span>
-        </p>
-        <p>
-          <span className="user_comment">
-            J'utilise les huiles moteur et les filtres de chez Future Trucks depuis des années et je n'ai jamais été déçu. Leurs produits m'ont aidé à assurer le bon fonctionnement et l'efficacité de
-            mon véhicule, et leurs prix sont très abordable.
-          </span>
-          <span className="user_name">Mr. NJOLEU Charles</span>
-        </p>
+      <div ref={ref}>
+        {/* <Comment delay={0} inView={isInView} />
+        <Comment delay={0.5} direction="right" inView={isInView} />
+        <Comment delay={1} inView={isInView} />
+        <Comment delay={1.5} direction="right" inView={isInView} /> */}
+        {...users.map(({ delay, direction }, i) => <Comment delay={i} direction={direction} id={`c${i}`} key={nanoid()} inView={isInView} />)}
       </div>
     </section>
   );
 };
 
-export const Comment = () => {
+interface CommentProps {
+  direction?: string;
+  inView?: boolean;
+  delay: number;
+  id: string;
+}
+
+export const Comment: React.FC<CommentProps> = ({ direction = "left", inView = false, delay, id }) => {
+  const t = useTranslations(`comments.${id}`);
+
   return (
-    <p>
-      <span className="user_comment">
-        J'utilise les huiles moteur et les filtres de chez Future Trucks depuis des années et je n'ai jamais été déçu. Leurs produits m'ont aidé à assurer le bon fonctionnement et l'efficacité de mon
-        véhicule, et leurs prix sont très abordable.
-      </span>
-      <span className="user_name">Mr. NJOLEU Charles</span>
-    </p>
+    <motion.p animate={{ x: inView ? "0%" : direction === "right" ? "-105%" : "105%" }} transition={{ duration: 0.5, ease: "easeInOut", delay: 0.5 * delay }}>
+      <span className="user_comment">{t("text")}</span>
+      <span className="user_name"> {t("user")}</span>
+    </motion.p>
   );
 };
 
